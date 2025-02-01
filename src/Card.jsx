07e89c638899b7cb7e31e9ad9f308d0f49b15ccc
dharaneesh-006 <dharaneesh112@gmail.com>
2025-02-01 22:13:34 +0530

    Sample Card test

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card()
+{
+    return(
+        <div>
+        <img src="my-react-app/src/assets/SPD.jpg" alt="" />
+        <h2>This is a Card</h2>
+        </div>
+    )
+}
+
+export default Card
