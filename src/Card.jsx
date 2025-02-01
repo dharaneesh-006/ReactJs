77ab889942a026db02e3e3f7e214cc5a41924f9a
Dharaneesh <137403057+dharaneesh-006@users.noreply.github.com>
2025-02-01 22:01:24 +0530

    Card test

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="" alt="Sample test" />
+            <h2>Code Loop</h2>
+            <p>This IS the test of top2 in Bro's Code</p>
+        </div>
+    )
+
+}
+
+export default Card;
